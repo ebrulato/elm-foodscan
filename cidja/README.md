e615a38fc5e494b19a38cf8f1ea56f05d371b9ac
ebrulato <eric.brulatout@gmail.com>
2018-06-22 23:10:15 +0200

    first commit

---
+++ cidja/README.md
@@ -0,0 +1,23 @@
+Front Office of the project.
+
+## Dev mode
+
+### Frontend
+
+Se placer dans le répertoire /cidja et lancer les commandes:
+
+```sh
+elm-package install
+bower install
+yarn install
+yarn dev:build
+```
+
+Puis démarrer l'application avec
+
+```sh
+yarn dev
+```
+
+
+Suivre les instructions pour se connecter à l'application en locale.
---
+++ cidja/src/index.js
@@ -0,0 +1,8 @@
+import { Main } from './Main.elm';
+
+//import '../bower_components/paper-spinner/paper-spinner.html';
+//import '../bower_components/paper-checkbox/paper-checkbox.html';
+//import '../bower_components/paper-input/paper-input.html';
+//import '../bower_components/paper-button/paper-button.html';
+
+Main.embed(document.getElementById('root'));
